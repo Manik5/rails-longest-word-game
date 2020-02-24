49b828c95e24365a5048b21e4466fcb333d7343a
Manik A A Ngurah <manik.aangurah@gmail.com>
2020-02-24 19:03:17 +0800

    Done

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,3 +1,6 @@
+require 'open-uri'
+
+
 class GamesController < ApplicationController
   def new
     @letters = Array('A'..'Z').sample(10)
@@ -5,9 +8,26 @@ class GamesController < ApplicationController
   end
 
   def score
-    # @letters = params[:grid]
-    # @user_input = params[:name]
-    url = https://wagon-dictionary.herokuapp.com/:word
-    @letters = JSON.parse
+    @letters = JSON.parse(params[:grid])
+    @user_input = params[:name].upcase
+    url = "https://wagon-dictionary.herokuapp.com/#{@user_input}"
+    response = JSON.parse(open(url).read)
+    # @letter = @letters.find { |letter| params[:id] == letter["id"] }
+    if !response["found"]
+      @result = "The word is not included in the wagon-dictionary"
+    elsif word_in_grid?(@letters, @user_input) == false
+      @result = "The word is not in the grid"
+    else
+      @result = "You won!"
+    end
+  end
+
+  def word_in_grid?(letters, word)
+    word = word.split("")
+    word.each do |letter|
+      return false if word.count(letter) > letters.count(letter)
+    end
+    true
   end
 end
+
